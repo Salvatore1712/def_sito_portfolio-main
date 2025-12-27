ec7c8defcf364717eaf00f18ee027984c037ae14
Salvatore De Roma <sdrsalvatore@gmail.com>
2025-12-27 09:02:26 +0300

    versione 3_addLink

--- JS/script_menu.js
+++ JS/script_menu.js
@@ -1,6 +1,7 @@
 let menu = document.querySelector(".nav-menu");
 let buttonMenu = document.querySelector(".menu-hamburger");
 
-buttonMenu.addEventListener("click", function() {
+buttonMenu.addEventListener("click", function(el) {
+    el.preventDefault();
     menu.classList.toggle("show-menu")
 })
